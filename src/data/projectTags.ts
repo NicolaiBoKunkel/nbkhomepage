720f58c9b5b7dafc7512be39cbcc9e60e28c9c17
Nicolai Bo Kunkel <nicolai@kunkel.dk>
2025-06-24 02:35:46 +0200

    .

--- src/data/projectTags.ts
+++ src/data/projectTags.ts
@@ -1,7 +1,7 @@
 export const projectTags: Record<string, string[]> = {
     'e-commerce-project': ['Node.js', 'Microservices', 'MongoDB', 'PostgreSQL', 'React', 'Next.js', 'RabbitMQ', 'Redis', 'Docker', 'Kubernetes', 'Grafana', 'Prometheus', 'JEST', 'JavaScript', 'TypeScript', 'Tailwind CSS'],
     'Flowmaster2': ['Next.js', 'React', 'TypeScript', 'Tailwind CSS', 'ReactFlow', 'Vercel'],
-    'nbkhomepage': ['Next.js', 'React', 'TypeScript', 'Vercel'],
+    'nbkhomepage': ['Next.js', 'React', 'TypeScript', 'Vercel', 'Tailwind CSS',],
     'nextjs-movie-backend': ['Node.js', 'JavaScript', 'MongoDB', 'JEST', 'Render', 'TMDB API'],
     'nextjs-moviedb': ['Next.js', 'React', 'TypeScript', 'Tailwind CSS', 'Vercel', 'Cypress'],
 };
