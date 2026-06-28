f7f369ba28a271c12dd9368da3009b9b705ed52c
Nicolai Bo Kunkel <nicolai@kunkel.dk>
2026-06-28 23:51:07 +0200

    added tags

--- src/data/projectTags.ts
+++ src/data/projectTags.ts
@@ -5,4 +5,5 @@ export const projectTags: Record<string, string[]> = {
     'nextjs-movie-backend': ['Node.js', 'JavaScript', 'MongoDB', 'JEST', 'Render', 'TMDB API'],
     'nextjs-moviedb': ['Next.js', 'React', 'TypeScript', 'Tailwind CSS', 'Vercel', 'Cypress', 'CI/CD'],
     'kanbanBoard': ['Vue 3', 'Vite', 'Vitest', 'TypeScript', 'Vercel', 'CI/CD', 'Vuetify'],
+    'flow-management-platform': ['Next.js', 'React', 'TypeScript', 'Tailwind CSS', 'ReactFlow', 'Vercel'],
 };
